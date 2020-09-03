c787856d2444a090bfee2251954766a686f5475c
MAG-BOSS <b18025@students.iitmandi.ac.in>
2020-09-03 18:53:28 +0530

    added chat Window

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -24,10 +24,42 @@ export default class Chat extends Component{
         try{
             db.ref("chats").on("value", snapshot=>{
                 let chats=[];
-                snapshot.forEach(snap)=>{
-                    chats
-                }
-            })
+                snapshot.forEach((snap)=>{
+                    chats.push(snap.val());
+                });
+                chats.sort(function(a,b){return a.timestamp - b.timestamp})
+                this.setState({chats});
+                chatArea.scrollBy(0,chatArea.scrollHeight);
+                this.setState({loadingChats:false});
+            });
+        }catch(error){
+            this.setState({readError:error.message,loadingChats:false});
         }
+    }
+    handleChange(event){
+        this.setState({
+            content:event.target.value
+        });
+    }
+    async handleSubmit(event){
+        event.preventDefault();
+        this.setState({writeError:null});
+        const chatArea=this.myRef.current;
+        try{
+            await db.ref("chats").push({
+                content:this.state.content,
+                timestamp:Date.now(),
+                uid:this.state.user.uid
+            });
+            this.setState({content:''});
+            chatArea.scrollBy(0,chatArea.scrollHeight);
+
+        }catch(error){
+            this.setState({writeError:error.message});
+        }
+    }
+    formatTime(timestamp){
+        const d=new Date(timestamp);
+        const time=
     }
 }
